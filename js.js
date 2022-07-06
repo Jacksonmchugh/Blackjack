f8cab17c99aa0d925d00fc1301456d957e56eee0
Jackson McHugh <jackson.mchugh@gmail.com>
2022-07-06 10:02:26 -0400

    css is done

--- js.js
+++ js.js
@@ -1,5 +1,21 @@
-var suits = ['spades', 'diamonds', 'clubs', 'hearts']
-var values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+const suits = ['spades', 'diamonds', 'clubs', 'hearts']
+const values = [
+  'A',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K'
+]
+let currentPlayer = 0
+
 function getDeck() {
   let deck = new Array()
   for (let i = 0; i, suits.length; i++) {
@@ -16,31 +32,31 @@ function getDeck() {
 }
 
 function shuffle() {
-  for (var i = 0; i < 500; i++) {
-    var location1 = Math.floor(Math.random() * deck.length)
-    var location2 = Math.floor(Math.random() * deck.length)
-    var hands = deck[location2]
+  for (var i = 0; i < 10; i++) {
+    let location1 = Math.floor(Math.random() * deck.length)
+    let location2 = Math.floor(Math.random() * deck.length)
+    let hands = deck[location2]
 
     deck[location1] = deck[location2]
     deck[location2] = hands
   }
 }
-var players = new Array()
+let players = new Array()
 function createPlayers(num) {
   players = new Array()
   for (var i = 1; i <= num; i++) {
-    var hand = new Array()
-    var player = { Name: 'Player ' + i, ID: i, Points: 0, Hand: hand }
+    let hand = new Array()
+    let player = { Name: 'Player ' + i, ID: i, Points: 0, Hand: hand }
     players.push(player)
   }
 }
 function createPlayersUI() {
   document.getElementById('players').innerHTML = ''
   for (var i = 0; i < players.length; i++) {
-    var div_player = document.createElement('div')
-    var div_playerid = document.createElement('div')
-    var div_hand = document.createElement('div')
-    var div_points = document.createElement('div')
+    let div_player = document.createElement('div')
+    let div_playerid = document.createElement('div')
+    let div_hand = document.createElement('div')
+    let div_points = document.createElement('div')
     div_points.className = 'points'
     div_points.id = 'points_' + i
     div_player.id = 'player_' + i
@@ -55,7 +71,7 @@ function createPlayersUI() {
   }
 }
 function startBlackjack() {
-  document.getElementById('btnStart').value = 'Restart'
+  document.getElementById('Restartbtn').value = 'Deal'
   document.getElementById('status').style.display = 'none'
 
   currentPlayer = 0
@@ -66,60 +82,63 @@ function startBlackjack() {
   dealHands()
   document.getElementById('player_' + currentPlayer).classList.add('active')
 }
-function dealHands(){
-    for (ver i = 0; i < 2; i++) 
-    {
-        for (var x = 0; x < players.length; x++){
-            var card = deck.pop();
-            players[x].Hand.push(card);
-            renderCard(card, x);
-            updatePoints();
-        }
+function dealHands() {
+  for (var i = 0; i < 2; i++) {
+    for (var x = 0; x < players.length; x++) {
+      let card = deck.pop()
+      players[x].Hand.push(card)
+      renderCard(card, x)
+      updatePoints()
     }
-    updateDeck();
+  }
+  updateDeck()
 }
-function renderCard(card, player){
-    var hand = document.getElementById('hand_' + player);
-    hand.appendChild(getCardUI(card));
+function renderCard(card, player) {
+  var hand = document.getElementById('hand_' + player)
+  hand.appendChild(getCardUI(card))
 }
-function getCardUI(card){
-    var el = document.createElement('div');
-    el.className = 'card';
-    el.innerHTML = card.Suit + ' ' + card.Value;
-    return el;
+function getCardUI(card) {
+  let el = document.createElement('div')
+  let icon = ''
+  if (card.Suit == 'Hearts') icon = '&hearts;'
+  else if (card.Suit == 'Spades') icon = '&spades;'
+  else if (card.Suit == 'Diamonds') icon = '&diamonds;'
+  else if (card.Suit == 'Hearts') icon = '&hearts;'
+  el.className = 'card'
+  el.innerHTML = card.Suit + ' ' + card.Value
+  return el
 }
-var currentPlayer = 0;
-function hitMe(){
-    var card = deck.pop();
-    players[currentPlayer].Hand.push(card);
-    renderCard(card, currentPlayer);
-    updatePoints();
-    check();
+let currentPlayer = 0
+function hitMe() {
+  let card = deck.pop()
+  players[currentPlayer].hand.push(card)
+  renderCard(card, currentPlayer)
+  updatePoints()
+  check()
 }
-function check(){
-    if(players[currentPlayer].Points > 21){
-        document.getElementById('status').innerHTML = 'Player: ' + players[currentPlayer].Id + ' Lost';
+function check() {
+  if (players[currentPlayer].Points > 21) {
+    document.getElementById('status').innerHTML =
+      'Player: ' + players[currentPlayer].Id + ' Lost'
+  }
+}
+function stay() {
+  if (currentPlayer != players.length - 1) {
+    document.getAnimations('player_' + currentPlayer).classList.remove('active')
+    currentPlayer += 1
+    document.getElementById('player_' + currentPlayer).classList.add('active')
+  } else {
+    end()
+  }
+}
+function end() {
+  let winner = -1
+  let score = 0
+  for (var i = 0; i < players.length; i++) {
+    if (players[i].Points > score && players[i] < 22) {
+      winner = i
     }
-}
-function stay(){
-    if(currentPlayer != players.length-1){
-        document.getAnimations('player_' + currentPlayer).classList.remove('active');
-        currentPlayer += 1;
-        document.getElementById('player_' + currentPlayer).classList.add('active');
-    }
-    else {
-        end();
-    }
-}
-function end(){
-    var winner = -1;
-    var score = 0;
-    for (var i =0; i < players.length; i++)
-    {
-        if (players[i].Points > score && players[i] < 22){
-            winner = i;
-        }
-        score = players[i].Points;
-    }
-    document.getElementById('status').innerHTML = "You won " + players[winner].ID;
+    score = players[i].Points
+  }
+  document.getElementById('status').innerHTML = 'You won ' + players[winner].ID
 }
